c064af9e2920223ae2e3920b6b76e5b6b939d4c2
aarispathan <140056433+aarispathan@users.noreply.github.com>
2024-12-26 11:47:24 +0000

    conversion of data types

---
+++ js_basic/javaConversionOperation3.js
@@ -0,0 +1,29 @@
+// NUMBER
+
+// let score = "458agdf";
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+// console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
+
+// "458" => 458
+// "458agdf" => NaN = not a number
+// true => 1; false => 0;
+
+ 
+// Boolean  TRUE-FALSE
+
+let isloggenIn = "AARIS"
+
+// "AARIS" TRUE
+// "" FALSE
+// 1 TRUE
+// 0 FALSE
+
+
+let booleanIsLoggedIn = Boolean(isloggenIn)
+console.log(booleanIsLoggedIn);
